1a18552914112f805c47dabc4a9c0c60192ad79e
Adrian Arcas <102859722+AdrianArcas@users.noreply.github.com>
2023-11-12 13:47:23 +0200

    Make gauss sum return 0 more often

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def gaussian(mean, coord, std_dev):
     return np.e ** (-((mean - coord) ** 2) / (2 * std_dev ** 2))
 
 
-def calculate_coord_with_gauss(mean, std_dev, gaussian_threshold):
+def calculate_coord_with_gauss(mean, std_dev):
     while True:
+        gaussian_threshold = int(random.uniform(0, 1) * 1000) / 1000.0
         coord = random.uniform(-300, 300)
-
         gaus_result = gaussian(mean, coord, std_dev)
-        if gaus_result > gaussian_threshold:
+        if gaus_result >= gaussian_threshold:
             return coord
 
 
@@ -27,20 +27,21 @@ class Zone:
     def scattered_points(self):
         points = []
         for _ in range(self.no_of_points):
-                # Generate a point according to the Gaussian distribution
-                gaussian_threshold = random.uniform(0, 1)
-                point_x = calculate_coord_with_gauss(self.mean_x, self.std_dev_x, gaussian_threshold)
-                point_y = calculate_coord_with_gauss(self.mean_y, self.std_dev_y, gaussian_threshold)
-                points.append((point_x, point_y))
+            point_x = calculate_coord_with_gauss(self.mean_x, self.std_dev_x)
+            point_y = calculate_coord_with_gauss(self.mean_y, self.std_dev_y)
+            points.append((point_x, point_y))
         return points
 
+class Centroid:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-# Create zones with different centers and spreads
-blue_zone = Zone(180, 220, 50, 50, 3333)
-red_zone = Zone(-100, 110, 75, 50, 3333)
-yellow_zone = Zone(210, -150, 25, 100, 3333)
 
-# Generate the scatter plot
+blue_zone = Zone(180, 220, 10, 10, 3333)
+red_zone = Zone(-100, 110, 20, 10, 3333)
+yellow_zone = Zone(210, -150, 20, 10, 3333)
+
 for zone, color in zip([blue_zone, red_zone, yellow_zone], ['b', 'r', 'y']):
     points = zone.scattered_points()
     x_coords, y_coords = zip(*points)
